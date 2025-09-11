d762d2cc337545fa18cd7435eb8e7c82e3d6f9ea
Thắng Phan Đức <apgpdt10@gmail.com>
2025-09-11 15:34:24 +0700

    123

---
+++ Lab_01_python/ex02/ex02_09.py
@@ -0,0 +1,13 @@
+def ktra_songuyento(n) :
+    if n <=1 :
+        return False
+    for i in range(2,int (n ** 0.5)+1) :
+        if n % i == 0 :
+            return False
+    return True
+number = int (input ("Nhap so can kiem tra : "))
+if ktra_songuyento(number) :
+    print (number , "la so nguyen to")
+else :
+    print (number , "khong phai la so nguyen to")
+    
---
+++ Lab_01_python/ex02/ex02_10.py
@@ -0,0 +1,4 @@
+def dao_nguoc_chuoi(s):
+    return s[::-1]
+s = input("nhap chuoi: ")
+print("chuoi dao nguoc la: ", dao_nguoc_chuoi(s))
